fab0672fa36266bd30b18fc211506bf51b0a9dab
thebravyone <thebravyone@gmail.com>
2023-10-20 17:26:43 -0300

    return empty array at guard clause

--- index.js
+++ index.js
@@ -1,43 +1,38 @@
 export default function evepaste(paste) {
+  if (typeof paste !== "string") return [];
 
-    if (typeof paste !== "string")
-        return;
+  const rows = paste.split("\n").map((r) => r.split("\t"));
 
-    const rows = paste.split("\n").map((r) => r.split("\t"));
-
-    return rows
-        .map(r => parseRow(r))
-        .filter(r => r !== undefined)
-        .reduce((aggregated, item) => {
-            const existingItem = aggregated.find((aggItem) => aggItem.typeName === item.typeName)
-            if (existingItem) {
-                existingItem.quantity += item.quantity;
-            } else {
-                aggregated.push({ ...item })
-            }
-            return aggregated
-        }, []);
+  return rows
+    .map((r) => parseRow(r))
+    .filter((r) => r !== undefined)
+    .reduce((aggregated, item) => {
+      const existingItem = aggregated.find(
+        (aggItem) => aggItem.typeName === item.typeName
+      );
+      if (existingItem) {
+        existingItem.quantity += item.quantity;
+      } else {
+        aggregated.push({ ...item });
+      }
+      return aggregated;
+    }, []);
 }
 
 function parseRow(row) {
+  if (typeof row[0] === "string" && row[0] !== "") {
+    // Multibuy totals
+    if (row[0] === "Total:") return;
 
-    if (typeof row[0] === "string" && row[0] !== '') {
+    //  Unpacked or Used charge
+    if (row[1] === "") return;
 
-        // Multibuy totals
-        if (row[0] === "Total:")
-            return
-
-        //  Unpacked or Used charge
-        if (row[1] === '')
-            return
-
-        // Item or Ship
-        const quantity = parseInt(row[1].replace('.', ''));
-        if (!isNaN(quantity) && !/[a-z]/i.test(row[1]))
-            return {
-                typeName: row[0].trim(), // trim to remove space padding in contracts of fitted ships
-                quantity: quantity
-            }
-    }
-
+    // Item or Ship
+    const quantity = parseInt(row[1].replace(".", ""));
+    if (!isNaN(quantity) && !/[a-z]/i.test(row[1]))
+      return {
+        typeName: row[0].trim(), // trim to remove space padding in contracts of fitted ships
+        quantity: quantity,
+      };
+  }
 }
